c0b000c287337d43c42f572b1384ce7a4b6b930d
Eavan <eavan.nicholson@gmail.com>
2022-01-28 14:25:24 -0500

    commit tree c1c8044d4cdeeafb3101063df56de5048b8fa750

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "0px";
+
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
@@ -6,7 +12,7 @@ function moveDodgerLeft() {
       dodger.style.left = `${left - 1}px`;
     }
   }
-  document.addEventListener("keydown", function (e) {
+  document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
     }
@@ -16,11 +22,11 @@ function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
 
-    if (left > 390) {
+    if (left > 0) {
         dodger.style.left = `${left + 1}px`;
     }
   }
-  document.addEventListener("keydown", function (e) {
+  document.addEventListener("keydown", function(e) {
       if (e.key === "ArrowRight") {
           moveDodgerRight();
       }
